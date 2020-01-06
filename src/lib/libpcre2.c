9e87debaac6df67eaad1fec450bbac52d43b8bf9
Stephen Riley <jstephen.riley@yahoo.com>
2020-01-06 10:06:10 -0800

    oops, uncomment the offender

--- src/lib/libpcre2.c
+++ src/lib/libpcre2.c
@@ -5,26 +5,26 @@
 
 // --------------------------------------------------------------------
 
-static PCRE2_SIZE _lastErrorOffset = 0;
+// static PCRE2_SIZE _lastErrorOffset = 0;
 
-EMSCRIPTEN_KEEPALIVE
-PCRE2_SIZE lastErrorOffset()
-{
-  return _lastErrorOffset;
-}
+// EMSCRIPTEN_KEEPALIVE
+// PCRE2_SIZE lastErrorOffset()
+// {
+//   return _lastErrorOffset;
+// }
 
 // --------------------------------------------------------------------
 
-// EMSCRIPTEN_KEEPALIVE
-// pcre2_match_data *match(
-//     pcre2_code *code,
-//     uint16_t *subject,
-//     PCRE2_SIZE length,
-//     PCRE2_SIZE offset)
-// {
-//   pcre2_match_data *result = pcre2_match_data_create_from_pattern(code, NULL);
+EMSCRIPTEN_KEEPALIVE
+pcre2_match_data *match(
+    pcre2_code *code,
+    uint16_t *subject,
+    PCRE2_SIZE length,
+    PCRE2_SIZE offset)
+{
+  pcre2_match_data *result = pcre2_match_data_create_from_pattern(code, NULL);
 
-//   pcre2_match(code, subject, length, offset, 0, result, NULL);
+  pcre2_match(code, subject, length, offset, 0, result, NULL);
 
-//   return result;
-// }
+  return result;
+}
